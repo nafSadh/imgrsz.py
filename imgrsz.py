be101fbf4e11a98966493f0af2279fc5e0f8676c
nafSadh <khan@nafsadh.com>
2015-03-19 00:34:11 -0400

    show usage() when no arg passed

--- imgrsz.py
+++ imgrsz.py
@@ -1,30 +1,31 @@
 # imgrsz.py -- IMaGe ReSiZe script in Python
 
-# Free software by nafSadh-khan (http://nafSadh.com) 
+# Free software by nafSadh-khan (http://nafSadh.com)
 
 # This is free and unencumbered software released into the public domain.
 #
-# Anyone is free to copy, modify, publish, use, compile, sell, or distribute 
-# this software, either in source code form or as a compiled binary, for any 
+# Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+# this software, either in source code form or as a compiled binary, for any
 # purpose, commercial or non-commercial, and by any means.
 #
-# In jurisdictions that recognize copyright laws, the author or authors of this 
-# software dedicate any and all copyright interest in the software to the 
-# public domain.  We make this dedication for the benefit of the public at 
-# large and to the detriment of our heirs and successors.  We intend this 
-# dedication to be an overt act of relinquishment in perpetuity of all present 
+# In jurisdictions that recognize copyright laws, the author or authors of this
+# software dedicate any and all copyright interest in the software to the
+# public domain.  We make this dedication for the benefit of the public at
+# large and to the detriment of our heirs and successors.  We intend this
+# dedication to be an overt act of relinquishment in perpetuity of all present
 # and future rights to this software under copyright law.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTBILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT, IN NO EVENT SHALL THE 
-# AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN 
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTBILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT, IN NO EVENT SHALL THE
+# AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # For more information, please refer to <http://unlicense.org/>
 
- 
+
+
 def image_resize(input, output, width, height, scale):
   import PIL
   from PIL import Image
@@ -55,7 +56,7 @@ def usage():
   print '                   ! required only when replacing'
   print ''
   print '  Sizing'
-  print '  -w, --width      target width of resized image' 
+  print '  -w, --width      target width of resized image'
   print '  -h, --height     target height of resized image'
   print '  -s, --scale      scaling factor of resized image'
   print '                   `if you provide either of w,h this program will compute'
@@ -78,16 +79,17 @@ def main():
     print(err) # will print something like 'option -a not recognized'
     usage()
     sys.exit(2)
-  
+  if len(options) < 1: usage(); sys.exit()
+
   # defaults
   input, output = '',''
   width, height = -1,-1
   scale = -1.0
-  replace = False  
+  replace = False
   quiet = False
   verbose = False
-  
-  # read args  
+
+  # read args
   for opt, arg in options:
     if opt in ('-i', '--input'):
       input = arg
@@ -110,7 +112,7 @@ def main():
       verbose = True
     else:
       assert False, 'unhandled option'
-      
+
   if verbose:
       print 'input:\t', input
       print 'output:\t', output
@@ -119,30 +121,30 @@ def main():
       print 'scale:\t', scale
       print 'replace:', replace
       print 'quiet:\t', quiet
-  
+
   if input == '':
     if not quiet: print '[!] pass an input image file to resize [-i, --input]'
     sys.exit()
   if replace:
     output = input
-    if not quiet: print 'replacing input image with output file' 
+    if not quiet: print 'replacing input image with output file'
   if output == '':
-    if not quiet:      
+    if not quiet:
       print '[!] pass an output file-name to write the resized image to [-o, --output]'
       print '    to replace original file with resized image, use [-r, --replace]'
     sys.exit()
   if width <0 and height<0 and scale<0.0:
     if not quiet:
       print '[!] pass atleast one of height, width or scale values:'
-      print '    -w, --width: width of resized image'  
-      print '    -h, --height: height of resized image'  
-      print '    -s, --scale: scale ratio for resized image'  
+      print '    -w, --width: width of resized image'
+      print '    -h, --height: height of resized image'
+      print '    -s, --scale: scale ratio for resized image'
       print '    if one passed, others are duly calculated'
     sys.exit()
   # ...
   width, height, w, h = image_resize(input, output, width, height, scale)
   if not quiet: print 'resized', input,'(',w,'X',h,')', 'to', output, '(',width,'X',height,')'
   return
-  
+
 if __name__ == '__main__':
   main()
